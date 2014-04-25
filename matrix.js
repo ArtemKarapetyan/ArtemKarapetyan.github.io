9a902463e12c6a7652c03d844a87ca46b28439b7
Artem Karapetyan <artem.karapetyan95@gmail.com>
2014-04-25 19:39:21 -0400

    changes

--- matrix.js
+++ matrix.js
@@ -1,42 +1,7 @@
-ASCII_BEGIN = 33;
-ASCII_END = 126;
-CHARS_PER_ROW = Math.floor($(window).width() / 8);
-TEXT_LENGTH = CHARS_PER_ROW * 2;
-REFRESH_FREQUENCY = 500;
 
 INPUT_HEAD = 'gordn:~ user$ ';
 
 
-	// regenerates content of the matrix
-	function refresh(data) {
-		for (var x=0; x < data.length; x++) {
-			data[x] = String.fromCharCode(Math.floor((Math.random() * (asciiEnd - asciiBegin)) + asciiBegin))
-		}
-		return data.join("");
-	}
-
-	// determine how many lines we need to fill window
-	h = $(window).height();
-	lineheight = $(".line").first().height()
-	lines = Math.ceil(h / lineheight)
-
-	for (var y=0; y < lines; y++) {
-		$("#lines").append($(".line").first().clone())
-	}
-
-	// array to contain matrix content
-	data = Array(textLength);
-	data.length = textLength;
-
-	window.setInterval(function() {
-		p = refresh(data);
-		// give each line a random slice of the pie
-		$(".line").each(function(index, ele) {
-			i = Math.floor(Math.random()* (textLength - charsPerRow))
-			$(ele).text(p.slice(i, i + charsPerRow))
-		})
-	}, refreshInterval);
-}
 
 function initConsole() {
 	// handle special keys:
